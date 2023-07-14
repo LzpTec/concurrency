f36e624e1b7764e817b758740d3d1e556abd1d2f
André Posso <admin@lzptec.com>
2023-07-14 01:39:22 -0300

    feat: Add `@lzptec/concurrency/batch` and `@lzptec/concurrency/concurrency` exports.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     treeshake: "smallest",
+    splitting: true,
     bundle: true,
     clean: true,
     outDir: 'dist',
     format: ['cjs', 'esm'],
-    entry: ['src/index.ts'],
+    entry: ['src/index.ts', 'src/batch.ts', 'src/concurrency.ts'],
     dts: true,
     minify: true,
     platform: 'neutral',
