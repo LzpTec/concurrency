cc8075c8ef56065b2cde82fd7f4b13f8ebe1d85d
André Posso <admin@lzptec.com>
2023-07-14 22:18:21 -0300

    test: Implement basic tests.

--- tests/batch.test.ts
+++ tests/batch.test.ts
@@ -1,1 +1,58 @@
 // TODO
+import test from 'ava';
+import { Batch } from '../src/batch';
+
+const BATCH_SIZE = 2;
+
+async function wait(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+test('Iterable', async t => {
+    function* test() {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+        return;
+    }
+
+    const calls: number[] = [];
+    await Batch.forEach({
+        input: test(),
+        batchSize: BATCH_SIZE,
+        task: async (value) => {
+            await wait(100);
+            calls.push(value);
+        }
+    });
+
+    t.deepEqual(calls, [1, 2, 3, 4]);
+    t.pass();
+});
+
+test('AsyncIterable', async t => {
+    async function* test() {
+        await wait(100);
+        yield 1;
+        await wait(100);
+        yield 2;
+        await wait(100);
+        yield 3;
+        await wait(100);
+        yield 4;
+        return;
+    }
+
+    const calls: number[] = [];
+    await Batch.forEach({
+        input: test(),
+        batchSize: BATCH_SIZE,
+        task: (value) => {
+            calls.push(value);
+        }
+    });
+
+    t.deepEqual(calls, [1, 2, 3, 4]);
+    t.pass();
+});
--- tests/concurrency.test.ts
+++ tests/concurrency.test.ts
@@ -1,1 +1,58 @@
 // TODO
+import test from 'ava';
+import { Concurrency } from '../src/concurrency';
+
+const MAX_CONCURRENCY = 2;
+
+async function wait(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+test('Iterable', async t => {
+    function* test() {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+        return;
+    }
+
+    const calls: number[] = [];
+    await Concurrency.forEach({
+        input: test(),
+        maxConcurrency: MAX_CONCURRENCY,
+        task: async (value) => {
+            await wait(100);
+            calls.push(value);
+        }
+    });
+
+    t.deepEqual(calls, [1, 2, 3, 4]);
+    t.pass();
+});
+
+test('AsyncIterable', async t => {
+    async function* test() {
+        await wait(100);
+        yield 1;
+        await wait(100);
+        yield 2;
+        await wait(100);
+        yield 3;
+        await wait(100);
+        yield 4;
+        return;
+    }
+
+    const calls: number[] = [];
+    await Concurrency.forEach({
+        input: test(),
+        maxConcurrency: MAX_CONCURRENCY,
+        task: (value) => {
+            calls.push(value);
+        }
+    });
+
+    t.deepEqual(calls, [1, 2, 3, 4]);
+    t.pass();
+});
