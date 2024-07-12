952bfd70d61876135e927b6180e335f009624590
André Posso <admin@lzptec.com>
2024-07-12 18:33:51 -0300

    test: Add Batch instance run test.

--- tests/batch.test.ts
+++ tests/batch.test.ts
@@ -3,12 +3,31 @@ import test from 'ava';
 import { Batch } from '../src/batch';
 
 const BATCH_SIZE = 2;
+const batchInstance = new Batch({
+    batchSize: BATCH_SIZE
+});
 
 async function wait(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-test('Iterable', async t => {
+test('run', async t => {
+    const calls: number[] = [];
+
+    for (let i = 1; i < 5; i++) {
+        const value = i;
+        await batchInstance.run(async () => {
+            await wait(value * 10);
+            calls.push(value);
+            return value;
+        });
+    }
+
+    t.deepEqual(calls, [1, 2, 3, 4]);
+    t.pass();
+});
+
+test('forEach', async t => {
     function* test() {
         yield 1;
         yield 2;
@@ -31,29 +50,47 @@ test('Iterable', async t => {
     t.pass();
 });
 
-test('AsyncIterable', async t => {
-    async function* test() {
-        await wait(50);
+test('map', async t => {
+    function* test() {
         yield 1;
-        await wait(50);
         yield 2;
-        await wait(50);
         yield 3;
-        await wait(50);
         yield 4;
         return;
     }
 
-    const calls: number[] = [];
-    await Batch.forEach({
+    const calls = await Batch.map({
         input: test(),
         batchSize: BATCH_SIZE,
         task: async (value) => {
             await wait(value * 10);
-            calls.push(value);
+            return value;
         }
     });
 
     t.deepEqual(calls, [1, 2, 3, 4]);
+    t.pass();
+});
+
+test('mapSettled', async t => {
+    function* test() {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+        return;
+    }
+
+    const calls = await Batch.mapSettled({
+        input: test(),
+        batchSize: BATCH_SIZE,
+        task: async (value) => {
+            await wait(value * 10);
+            return value;
+        }
+    });
+
+    const result = calls.filter(x => x.status === 'fulfilled').map(x => x.value);
+    t.deepEqual(result, [1, 2, 3, 4]);
     t.pass();
 });
